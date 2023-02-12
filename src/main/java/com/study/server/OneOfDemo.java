6b5e78c01cc04a66deff6b7f6c9fb63f62293d54
masaki nakamura <gmk4n0n@gmail.com>
2023-02-12 23:53:02 +0900

    protobuf oneof statement

---
+++ src/main/java/com/study/server/OneOfDemo.java
@@ -0,0 +1,40 @@
+package com.study.server;
+
+import com.study.mdoels.Credentials;
+import com.study.mdoels.EmailCredentials;
+import com.study.mdoels.PhoneOTP;
+
+public class OneOfDemo {
+
+    public static void main(String[] args) {
+        EmailCredentials emailCredentials = EmailCredentials.newBuilder()
+                                            .setEmail("abc@abc.com")
+                                            .setPassword("admin123")
+                                            .build();
+
+        PhoneOTP phoneOTP = PhoneOTP.newBuilder()
+                            .setNumber(1231231234)
+                            .setCode(456)
+                            .build();
+
+        // set only 1: emailmode or phonemode
+        Credentials credentials = Credentials.newBuilder()
+                                  .setEmailMode(emailCredentials)
+                                  //.setPhoneMode(phoneOTP)
+                                  .build();
+
+        login(credentials);
+    }
+
+    private static void login (Credentials credentials) {
+
+        switch (credentials.getModeCase()) {
+            case EMAILMODE:
+                System.out.println(credentials.getEmailMode());
+                break;
+            case PHONEMODE:
+                System.out.println(credentials.getPhoneMode());
+                break;
+        }
+    }
+}
