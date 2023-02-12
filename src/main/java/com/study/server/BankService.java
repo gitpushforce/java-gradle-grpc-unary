c16cad35b23c3ce1cad0768c92e53190811fc112
masaki nakamura <gmk4n0n@gmail.com>
2023-02-13 03:32:20 +0900

    adding o.grpc.stub package dependency to gradle

--- src/main/java/com/study/server/BankService.java
+++ src/main/java/com/study/server/BankService.java
@@ -1,6 +1,13 @@
 package com.study.server;
 
+import com.study.models.Balance;
+import com.study.models.BalanceCheckRequest;
 import com.study.models.BankServiceGrpc;
+import io.grpc.stub.StreamObserver;
 
-public class BankService extends BankServiceGrpc.BankServiceImplBase{
+public class BankService extends BankServiceGrpc.BankServiceImplBase {
+    @Override
+    public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
+        super.getBalance(request, responseObserver);
+    }
 }
