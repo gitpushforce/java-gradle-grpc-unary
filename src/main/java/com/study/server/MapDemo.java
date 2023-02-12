153124e79c0d08b2f8f49b795530818cc8d681d8
masaki nakamura <gmk4n0n@gmail.com>
2023-02-12 19:04:02 +0900

    maps in protobuf

---
+++ src/main/java/com/study/server/MapDemo.java
@@ -0,0 +1,38 @@
+package com.study.server;
+
+import com.study.models.Car;
+import com.study.models.Dealer;
+
+public class MapDemo {
+    public static void main(String[] args) {
+
+        Car hondaAccord = Car.newBuilder()
+                .setMaker("Honda")
+                .setModel("Accord")
+                .setYear(2020)
+                .build();
+
+        Car hondaCivic = Car.newBuilder()
+                .setMaker("Honda")
+                .setModel("Civic")
+                .setYear(2005)
+                .build();
+
+        Dealer dealer = Dealer.newBuilder()
+                .putModel(2005, hondaCivic)
+                .putModel(2020, hondaAccord)
+                .build();
+        System.out.println(
+                // throws IllegalArgumentException if key is not found
+                //dealer.getModelOrThrow(2004)
+
+                // returns a default model if key is not found
+                // dealer.getModelOrDefault(2019, hondaAccord)
+
+                // returns all elements of the map
+                dealer.getModelMap()
+        );
+
+
+    }
+}
